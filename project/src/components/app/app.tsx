8cdd6e4edf86a6ae19ce6d12bb9cc7d8ef035f1f
Darvetra <darvetra13@gmail.com>
2022-07-17 19:50:27 +0300

    добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-import MainScreen from '../../pages/main-screen/main-screen';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+
 import {HotelType} from '../../types/hotel';
+import {AppRoute} from '../../const';
+
+import MainScreen from '../../pages/main-screen/main-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import PropertyScreen from '../../pages/property-screen/property-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppScreenProps = {
   hotels: HotelType[];
@@ -7,7 +15,30 @@ type AppScreenProps = {
 
 function App({hotels}: AppScreenProps): JSX.Element {
   return (
-    <MainScreen hotels={hotels}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen hotels={hotels} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesScreen />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<PropertyScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/const.ts
@@ -0,0 +1,6 @@
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Room = '/offer/:id',
+}
