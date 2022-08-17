4ceebb3704ae1b75932ec664d1e1786934ead0b3
Darvetra <darvetra13@gmail.com>
2022-08-17 22:55:10 +0300

    создает и подключает компоненты Review и ReviewList к компоненту RoomScreen

--- project/src/pages/room-screen/room-screen.tsx
+++ project/src/pages/room-screen/room-screen.tsx
@@ -1,6 +1,7 @@
 import {useLocation} from 'react-router-dom';
 
 import Header from '../../components/header/header';
+import ReviewsList from '../../components/reviews-list/reviews-list';
 import ReviewForm from '../../components/review-form/review-form';
 import Map from '../../components/map/map';
 import OffersList from '../../components/offers-list/offers-list';
@@ -10,6 +11,8 @@ import {MapSettings} from '../../types/map';
 
 import {calcRatingWidth} from '../../utils';
 import {OfferCardStyles} from '../../const';
+
+import {reviews} from '../../mocks/reviews';
 
 type RoomScreenProps = {
   offers: OfferType[],
@@ -23,6 +26,7 @@ function RoomScreen(props: RoomScreenProps): JSX.Element {
   const urlId = Number(location.pathname.split('/').slice(-1));
   const offer: OfferType | undefined = offers.find((item) => item.id === urlId);
   const offerNeighbourhood = offers.slice(0, 3);
+  const reviewsCount = reviews.length;
 
   return (
     <div className="page">
@@ -117,32 +121,11 @@ function RoomScreen(props: RoomScreenProps): JSX.Element {
                 </div>
               </div>
               <section className="property__reviews reviews">
-                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-                <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
-                      </div>
-                      <span className="reviews__user-name">
-                        Max
-                      </span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{width: '80%'}} />
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.
-                        The building is green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                    </div>
-                  </li>
-                </ul>
+                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviewsCount}</span></h2>
+
+                <ReviewsList
+                  reviews={reviews}
+                />
 
                 <ReviewForm />
 
